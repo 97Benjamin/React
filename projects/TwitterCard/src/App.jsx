c858b0b3e6d01528461ff954c463218cf0a936bf
Benjamin Araneda <aaranedaa@gmail.com>
2024-10-29 13:34:19 -0300

    twitter card

---
+++ projects/TwitterCard/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      userName: 'pheralb',
+      name: 'Pablo H.',
+      isFollowing: false
+    },
+    {
+      userName: 'PacoHdezs',
+      name: 'Paco Hdez',
+      isFollowing: true
+    },
+    {
+      userName: 'TMChein',
+      name: 'Tomas',
+      isFollowing: false
+    }
+  ]
+
+export function App (){
+    return(
+        <section className='App'>
+        <TwitterFollowCard userName="elonmusk" children="Elon musk"/>
+        <TwitterFollowCard userName="midudev" children="Miguel Angel Duran"/>
+        </section>
+    )
+}
